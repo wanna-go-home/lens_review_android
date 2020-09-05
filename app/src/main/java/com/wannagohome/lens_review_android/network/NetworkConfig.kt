481267f33f4653865cf45cb3fb089ac0cdc5af67
rlarlvy153 <rlarlvy153@naver.com>
2020-09-05 10:08:11 +0900

    network config

--- app/src/main/java/com/wannagohome/lens_review_android/network/NetworkConfig.kt
+++ app/src/main/java/com/wannagohome/lens_review_android/network/NetworkConfig.kt
@@ -2,6 +2,6 @@ package com.wannagohome.lens_review_android.network
 
 object NetworkConfig {
 
-    const val API_BASE_URL = "http://ec2-54-180-109-166.ap-northeast-2.compute.amazonaws.com:8080/"
+    const val API_BASE_URL = "http://15.165.122.0:8080/"
     const val ALL_TIMEOUT = 3L
 }
