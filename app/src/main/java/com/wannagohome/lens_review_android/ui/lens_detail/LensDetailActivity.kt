e070819e04a5be1059fbd1f098b3b33793a59bd2
rlarlvy153 <rlarlvy153@naver.com>
2020-09-24 18:06:56 +0900

    lens detail api without UI

--- app/src/main/java/com/wannagohome/lens_review_android/ui/lens_detail/LensDetailActivity.kt
+++ app/src/main/java/com/wannagohome/lens_review_android/ui/lens_detail/LensDetailActivity.kt
@@ -3,8 +3,10 @@ package com.wannagohome.lens_review_android.ui.lens_detail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.wannagohome.lens_review_android.R
 import kotlinx.android.synthetic.main.activity_detailed_lens.*
+import kotlinx.android.synthetic.main.lens_list_item.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -14,7 +16,7 @@ class LensDetailActivity : AppCompatActivity() {
         const val DETAILED_LENS_ID = "lensId"
     }
 
-    private val lensDetailViewModel : LensDetailViewModel by viewModel()
+    private val lensDetailViewModel: LensDetailViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +35,17 @@ class LensDetailActivity : AppCompatActivity() {
         lensDetailViewModel.getLensDetail(lensId)
     }
 
-    private fun observeEvent(){
-        lensDetailViewModel.detailedLens.observe(this, Observer {
-            lensName.text = it.name
+    private fun observeEvent() {
+        lensDetailViewModel.lensName.observe(this, Observer {
+            lensName.text = it
+        })
+
+        lensDetailViewModel.lensPrice.observe(this, Observer {
+            lensPrice.text = it
+        })
+
+        lensDetailViewModel.productImage.observe(this, Observer {
+            Glide.with(this).load(it).into(lensProductImage)
         })
     }
 }
