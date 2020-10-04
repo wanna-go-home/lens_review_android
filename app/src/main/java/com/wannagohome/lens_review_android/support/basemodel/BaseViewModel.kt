954e8b342560391b73daa0263ec5bf1c77306e53
rlarlvy153 <rlarlvy153@naver.com>
2020-10-04 22:14:28 +0900

    review preview list

--- app/src/main/java/com/wannagohome/lens_review_android/support/basemodel/BaseViewModel.kt
+++ app/src/main/java/com/wannagohome/lens_review_android/support/basemodel/BaseViewModel.kt
@@ -2,8 +2,9 @@ package com.wannagohome.lens_review_android.support.basemodel
 
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
+import org.koin.core.KoinComponent
 
-open class BaseViewModel : ViewModel() {
+open class BaseViewModel : ViewModel(), KoinComponent {
 
     val disposable = CompositeDisposable()
 
---
+++ app/src/main/java/com/wannagohome/lens_review_android/ui/review/ReviewListAdapter.kt
@@ -0,0 +1,53 @@
+package com.wannagohome.lens_review_android.ui.review
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.wannagohome.lens_review_android.R
+import com.wannagohome.lens_review_android.network.model.ReviewPreview
+import kotlinx.android.synthetic.main.review_list_item.view.*
+
+class ReviewListAdapter(val itemClickListener: OnItemClickListener? = null) : RecyclerView.Adapter<ReviewListAdapter.BookListViewHolder>() {
+
+    var reviewPreviewList = listOf<ReviewPreview>()
+        set(shops) {
+            field = shops
+
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookListViewHolder {
+        return BookListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.review_list_item, parent, false))
+    }
+
+    override fun getItemCount() = reviewPreviewList.size
+
+    override fun onBindViewHolder(holder: BookListViewHolder, position: Int) {
+        holder.bind(reviewPreviewList[position])
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(clickedReviewPreview: ReviewPreview)
+    }
+
+    inner class BookListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var currentReviewPreview: ReviewPreview
+
+        init {
+            itemView.setOnClickListener {
+                itemClickListener?.onItemClick(currentReviewPreview)
+            }
+        }
+
+        fun bind(reviewPreview: ReviewPreview) {
+            currentReviewPreview = reviewPreview
+
+            itemView.reviewTitle.text = reviewPreview.title
+
+            itemView.reviewContent.text = reviewPreview.content
+        }
+
+
+    }
+}
