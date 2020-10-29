1974c78409ebf2494f5d89a8cc2bad98b2fa62e7
rlarlvy153 <rlarlvy153@naver.com>
2020-10-29 15:00:09 +0900

    sign up

--- app/src/main/java/com/wannagohome/lens_review_android/ui/login/LoginActivity.kt
+++ app/src/main/java/com/wannagohome/lens_review_android/ui/login/LoginActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import androidx.lifecycle.Observer
 import com.wannagohome.lens_review_android.R
 import com.wannagohome.lens_review_android.ui.MainActivity
+import com.wannagohome.lens_review_android.ui.signup.SignUpActivity
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_login.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -21,6 +23,10 @@ class LoginActivity : AppCompatActivity() {
             //TODO id pw 검증로직
             loginViewModel.login(userId.text.toString(), userPw.text.toString())
         }
+        signUp.setOnClickListener {
+            val intent = Intent(this@LoginActivity,SignUpActivity::class.java)
+            startActivity(intent)
+         }
 
         loginViewModel.loginSuccess.observe(this, Observer {
             if(it){
