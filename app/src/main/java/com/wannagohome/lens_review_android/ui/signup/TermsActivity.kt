aa95d3c89b865e17ed2e1ebb2ade0a80999fecad
rlarlvy153 <rlarlvy153@naver.com>
2021-01-31 22:45:35 +0900

    terms host

--- app/src/main/java/com/wannagohome/lens_review_android/ui/signup/TermsActivity.kt
+++ app/src/main/java/com/wannagohome/lens_review_android/ui/signup/TermsActivity.kt
@@ -2,13 +2,14 @@ package com.wannagohome.lens_review_android.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wannagohome.lens_review_android.BuildConfig
 import com.wannagohome.lens_review_android.R
 import com.wannagohome.lens_review_android.databinding.ActivityTermsBinding
 
 class TermsActivity : AppCompatActivity() {
 
     companion object{
-        const val TERMS_URL = "http://wy0105.iptime.org:8080/terms.html"
+        const val TERMS_URL = "${BuildConfig.API_HOST}/terms.html"
     }
 
     lateinit var binding : ActivityTermsBinding
