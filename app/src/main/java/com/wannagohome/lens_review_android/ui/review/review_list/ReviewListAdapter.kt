38daf1bc2d8afca43c5dcac678c4772906155f82
up0617 <up0617@gmail.com>
2021-02-15 16:26:36 +0900

    apply like on review board

--- app/src/main/java/com/wannagohome/lens_review_android/ui/review/review_list/ReviewListAdapter.kt
+++ app/src/main/java/com/wannagohome/lens_review_android/ui/review/review_list/ReviewListAdapter.kt
@@ -4,51 +4,61 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.jakewharton.rxbinding4.view.clicks
 import com.wannagohome.lens_review_android.databinding.ReviewListItemBinding
 import com.wannagohome.lens_review_android.network.model.helper.dateHelper
-import com.wannagohome.lens_review_android.network.model.review.ReviewPreview
+import com.wannagohome.lens_review_android.network.model.review.Review
 import com.wannagohome.lens_review_android.support.baseclass.BaseSimpleAdapter
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 
-class ReviewListAdapter : BaseSimpleAdapter<ReviewPreview, ReviewListAdapter.BookListViewHolder>() {
+class ReviewListAdapter : BaseSimpleAdapter<Review, ReviewListAdapter.LenseListViewHolder>() {
 
+    var onLikeClick: ((Int) -> Unit)? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookListViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LenseListViewHolder {
         val binding = ReviewListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return BookListViewHolder(binding)
+        return LenseListViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: BookListViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: LenseListViewHolder, position: Int) {
         holder.bind(items[position])
     }
 
-    inner class BookListViewHolder(private val itemBinding: ReviewListItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+    inner class LenseListViewHolder(private val itemBinding: ReviewListItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
 
         init {
-            itemView.setOnClickListener {
-                onItemClick?.invoke(adapterPosition)
+            itemBinding.mainContentView.setOnClickListener {
+                onItemClick?.invoke(absoluteAdapterPosition)
             }
+            itemBinding.likesIcon.clicks()
+                .observeOn(AndroidSchedulers.mainThread())
+                .throttleFirst(300, TimeUnit.MILLISECONDS)
+                .subscribe {
+                    onLikeClick?.invoke(absoluteAdapterPosition)
+                }
         }
 
-        fun bind(reviewPreview: ReviewPreview) {
+        fun bind(review: Review) {
 
-            itemBinding.reviewTitle.text = reviewPreview.title
+            itemBinding.reviewTitle.text = review.title
 
-            itemBinding.reviewContents.text = reviewPreview.content
+            itemBinding.reviewContents.text = review.content
 
-            itemBinding.pageviewNum.text = reviewPreview.viewCnt.toString()
+            itemBinding.pageviewNum.text = review.viewCnt.toString()
 
-            itemBinding.commentNum.text = reviewPreview.replyCnt.toString()
+            itemBinding.commentNum.text = review.replyCnt.toString()
 
-            itemBinding.likesIcon.isChecked = reviewPreview.isLiked
+            itemBinding.likesIcon.isChecked = review.isLiked
 
-            itemBinding.likeNum.text = reviewPreview.likeCnt.toString()
+            itemBinding.likeNum.text = review.likeCnt.toString()
 
-            itemBinding.reviewWriter.text = reviewPreview.nickname
+            itemBinding.reviewWriter.text = review.nickname
 
-            itemBinding.time.text = dateHelper.calcCreatedBefore(reviewPreview.createdAt)
+            itemBinding.time.text = dateHelper.calcCreatedBefore(review.createdAt)
 
-            Glide.with(itemView.context).load(reviewPreview.lens.productImages[0])
+            Glide.with(itemView.context).load(review.lens.productImages[0])
                 .into(itemBinding.lensImage)
 
 
--- app/src/main/java/com/wannagohome/lens_review_android/ui/review/review_list/ReviewPreviewViewModel.kt
+++ app/src/main/java/com/wannagohome/lens_review_android/ui/review/review_list/ReviewPreviewViewModel.kt
@@ -2,18 +2,17 @@ package com.wannagohome.lens_review_android.ui.review.review_list
 
 import androidx.lifecycle.MutableLiveData
 import com.wannagohome.lens_review_android.network.lensapi.LensApiClient
-import com.wannagohome.lens_review_android.network.model.review.ReviewPreview
 import com.wannagohome.lens_review_android.support.baseclass.BaseViewModel
 import com.wannagohome.lens_review_android.extension.addTo
+import com.wannagohome.lens_review_android.network.model.review.Review
 import org.koin.core.inject
-import timber.log.Timber
 
 
 class ReviewPreviewViewModel : BaseViewModel(){
 
     private val lensApiClient : LensApiClient by inject()
 
-    val reviewPreviewList = MutableLiveData<List<ReviewPreview>>()
+    val reviewPreviewList = MutableLiveData<List<Review>>()
 
     fun fetchReviewPreview(){
         lensApiClient.getAllReviews()
@@ -24,6 +23,36 @@ class ReviewPreviewViewModel : BaseViewModel(){
                     reviewPreviewList.value = it
                 }
             }.addTo(compositeDisposable)
+    }
+
+    fun like(articleId: Int) {
+        lensApiClient.postReviewLike(articleId)
+            .subscribe( {
+                val newReview = it.body()
+                var oldArticleList = reviewPreviewList.value?.toMutableList()
+                if (oldArticleList!=null && newReview !=null){
+                    val idx = oldArticleList.indexOfFirst { review ->  review.id == newReview.id }
+                    oldArticleList[idx] = newReview
+                }
+                reviewPreviewList.value = oldArticleList
+            }, {
+            })
+            .addTo(compositeDisposable)
+    }
+
+    fun unlike(articleId: Int) {
+        lensApiClient.deleteReviewLike(articleId)
+            .subscribe( {
+                val newReview = it.body()
+                var oldArticleList = reviewPreviewList.value?.toMutableList()
+                if (oldArticleList!=null && newReview !=null){
+                    val idx = oldArticleList.indexOfFirst { review ->  review.id == newReview.id }
+                    oldArticleList[idx] = newReview
+                }
+                reviewPreviewList.value = oldArticleList
+            }, {
+            })
+            .addTo(compositeDisposable)
     }
 
 }
