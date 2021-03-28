b91aa3e42bd8e3a9669fe748fe1ab04ef4ceb7f6
rlarlvy153 <rlarlvy153@naver.com>
2021-03-28 17:01:48 +0900

    메인탭 디자인

--- app/src/main/java/com/wannagohome/viewty/ui/MainActivity.kt
+++ app/src/main/java/com/wannagohome/viewty/ui/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.wannagohome.viewty.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
+import com.google.android.material.tabs.TabLayout
+import com.jakewharton.rxbinding4.view.clicks
+import com.wannagohome.viewty.R
 import com.wannagohome.viewty.databinding.ActivityMainBinding
+import com.wannagohome.viewty.extension.addTo
+import com.wannagohome.viewty.extension.gone
+import com.wannagohome.viewty.extension.visible
+import com.wannagohome.viewty.support.baseclass.BaseAppCompatActivity
+import com.wannagohome.viewty.ui.article.write.WriteArticleActivity
+import com.wannagohome.viewty.ui.review.write.WriteReviewActivity
 import org.koin.core.KoinComponent
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity(), KoinComponent {
+class MainActivity : BaseAppCompatActivity(), KoinComponent {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val iconEnumArray = TabIconEnum.values()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +31,104 @@ class MainActivity : AppCompatActivity(), KoinComponent {
 
         initViewPager()
 
+        initMainTab()
+
+        initWriteButton()
+
+
+    }
+
+    private fun initMainTab() {
+        val unselectedFilterColor =
+            ContextCompat.getColor(this@MainActivity, R.color.main_tab_unselected)
+        val selectedFilterColor =
+            ContextCompat.getColor(this@MainActivity, R.color.main_tab_selected)
+
+        for (icon in iconEnumArray) {
+            val v = binding.mainTabLayout.newTab()
+            v.icon = ContextCompat.getDrawable(this, icon.icon)
+            v.icon?.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+                unselectedFilterColor,
+                BlendModeCompat.SRC_IN
+            )
+
+            binding.mainTabLayout.addTab(v)
+        }
+
+        binding.mainTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab) {
+                p0.icon?.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+                    unselectedFilterColor,
+                    BlendModeCompat.SRC_IN
+                )
+            }
+
+            override fun onTabSelected(p0: TabLayout.Tab) {
+                p0.icon?.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+                    selectedFilterColor,
+                    BlendModeCompat.SRC_IN
+                )
+
+                binding.mainViewPager.run {
+                    setCurrentItem(p0.position, false)
+                }
+            }
+        })
+
+        val firstTab = binding.mainTabLayout.getTabAt(0)
+        binding.mainTabLayout.selectTab(firstTab)
+        firstTab?.icon?.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+            selectedFilterColor,
+            BlendModeCompat.SRC_IN
+        )
+
+    }
+
+    private fun initWriteButton() {
+        binding.writeBtn.post {
+            var offset = resources.displayMetrics.widthPixels / 5f / 2f
+            offset -= binding.writeBtn.width / 2f
+
+            binding.writeBtn.translationX = -offset
+        }
+
+        binding.writeBtn.clicks()
+            .subscribe {
+                if (binding.writeMenuLayout.visibility == View.VISIBLE) {
+                    binding.writeMenuLayout.gone()
+                } else {
+                    binding.writeMenuLayout.visible()
+                }
+            }.addTo(compositeDisposable)
+
+        binding.writeMenuLayout.clicks()
+            .subscribe {
+                binding.writeMenuLayout.gone()
+            }.addTo(compositeDisposable)
+
+        binding.writeReviewBtn.clicks()
+            .subscribe {
+                startActivityFromRight(this@MainActivity, WriteReviewActivity::class.java)
+                binding.writeMenuLayout.gone()
+
+            }.addTo(compositeDisposable)
+
+        binding.writeArticleBtn.clicks()
+            .subscribe {
+                startActivityFromRight(this@MainActivity, WriteArticleActivity::class.java)
+                binding.writeMenuLayout.gone()
+
+            }.addTo(compositeDisposable)
+
     }
 
     private fun initViewPager() {
-        binding.mainViewPager.adapter = MainViewPagerAdapter(supportFragmentManager)
-        binding.mainTabLayout.setupWithViewPager(binding.mainViewPager)
+        binding.mainViewPager.adapter = MainViewPagerAdapter(this)
+        binding.mainViewPager.isUserInputEnabled = false
     }
 }
--- app/src/main/java/com/wannagohome/viewty/ui/MainViewPagerAdapter.kt
+++ app/src/main/java/com/wannagohome/viewty/ui/MainViewPagerAdapter.kt
@@ -1,8 +1,10 @@
 package com.wannagohome.viewty.ui
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.wannagohome.viewty.AppComponents
 import com.wannagohome.viewty.R
 import com.wannagohome.viewty.ui.article.list.TabArticle
@@ -11,10 +13,36 @@ import com.wannagohome.viewty.ui.review.review_list.TabReview
 import com.wannagohome.viewty.ui.search_lens.TabSearch
 
 
-class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    private val fragmentTitleList = AppComponents.applicationContext.resources.getStringArray(R.array.main_tab_items)
+//class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+//    private val fragmentTitleList = AppComponents.applicationContext.resources.getStringArray(R.array.main_tab_items)
+//
+//    override fun getItem(position: Int): Fragment {
+//        return when (position) {
+//            1-> TabReview.newInstance()
+//            2 -> TabArticle.newInstance()
+//            3 -> TabMypage.newInstance()
+//            else -> TabSearch.newInstance()
+//        }
+//    }
+//
+//    override fun getPageTitle(position: Int): CharSequence? {
+//        return fragmentTitleList[position]
+//    }
+//
+//    override fun getCount(): Int {
+//        return fragmentTitleList.size
+//    }
+//}
 
-    override fun getItem(position: Int): Fragment {
+
+class MainViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+
+    override fun getItemCount(): Int {
+        return TabIconEnum.values().size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+
         return when (position) {
             1-> TabReview.newInstance()
             2 -> TabArticle.newInstance()
@@ -23,11 +51,4 @@ class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAV
         }
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return fragmentTitleList[position]
-    }
-
-    override fun getCount(): Int {
-        return fragmentTitleList.size
-    }
 }
